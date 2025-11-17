6e1d1143fc0d2e912a86ada5d22621eddb45a89f
lakikishorsubba <lakikishor7@gmail.com>
2025-11-17 15:12:27 +0600

    update:all authentications, add:fakeracksession

--- backend_rails_api/config/routes.rb
+++ backend_rails_api/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get "up" => "rails/health#show", as: :rails_health_check,
-  
-  controller: {
-    sessions: "users/sessions",
-    registrations: "users/registrations"
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
-
 end
