30202c3de4e257e3ed38bae931aba0391f61cf85
lakikishorsubba <lakikishor7@gmail.com>
2025-11-17 14:38:37 +0600

    update: authentication and devise jwt

--- backend_rails_api/config/routes.rb
+++ backend_rails_api/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guidesgit .rubyonrails.org/routing.html
+  get "up" => "rails/health#show", as: :rails_health_check,
+  
+  controller: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
