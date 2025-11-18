56c1613d6f4999dba14855c61916c322d40c45b4
lakikishorsubba <lakikishor7@gmail.com>
2025-11-18 17:22:35 +0600

    update:registration logic

--- backend_rails_api/app/controllers/users/sessions_controller.rb
+++ backend_rails_api/app/controllers/users/sessions_controller.rb
@@ -11,6 +11,7 @@ class Users::SessionsController < Devise::SessionsController
       }
     }, status: :ok
   end
+  
   #logout section, destroy session
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
@@ -32,31 +33,4 @@ class Users::SessionsController < Devise::SessionsController
   
     end
   end
-
-
-
-
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
